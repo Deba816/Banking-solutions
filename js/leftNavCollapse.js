ffcfc025d206b68c1cd81d9b8c3c04d4dbd4fc6a
Deba816 <debashis.mohakud@omc.com>
2026-05-19 15:59:36 +0530

    Delete js directory

--- js/leftNavCollapse.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    var sideBarWidth = $('.respSideBar').width();
-    console.log(sideBarWidth);
-    $('.responsiveLevtNav').on('click', () => {
-        $('.respSideBar').toggle("slide");
-        $('body').toggleClass("noScroll");
-    })
-})
